40bc82aa90eea0713fa86ea3cb50bf6a596b537c
Sv4k <vanes.sav4enko@gmail.com>
2019-05-15 07:39:11 +0700

    client update

--- client.py
+++ client.py
@@ -1,25 +1,33 @@
-import recogniser.face_recogniser
-import screenshot.screenshoter
-import dawn_mode
+import recogniser.face_recogniser as camera
+import screenshot.screenshoter as screen
+import dawn_mode as listener
 from active_proc import *
 import datetime
 import threading
 import time
+import requests
 
 #get_active_window()
-time_hist = []
+active_hist = []
+photo = camera.make_photo()
+screenshot = screen.get_screen_shot()
+clicks = []
+
 def active_progs():
-    global time_hist
+    global active_hist
     prev_active = get_active_window()
     start = datetime.datetime.now()
     while True:
         active_window = get_active_window()
         if prev_active != active_window:
-            finish = time.time()
-            if prev_active in time_hist.keys():
-                time_hist[prev_active] += finish - start
+            finish = datetime.datetime.now()
+            for i in active_hist:
+                if prev_active == i['proc']:
+                    delta = finish - start
+                    i['time'] += float(delta.seconds) / 60.0
+                    break
             else:
-                time_hist[prev_active] = finish - start
+                prev_active.append({"proc": prev_active, "time": float((finish - start).seconds) / 60.0})
             start = finish
             prev_active = active_window
         time.sleep(5)
@@ -27,15 +35,40 @@ def active_progs():
 
 def clicker():
     #если кликов нет 20 минут, то делаем скриншот и камеру
-    pass
+    global clicks
+    while True:
+        listener.start()
+        time.sleep(30)
+        clicks = listener.stop()
 
 
 def get_photo():
-    pass
+    user_photo = camera.make_photo()
+    return user_photo
+
+
+def main():
+    global active_hist, photo, screenshot, clicks
+    with open('start.txt', 'r') as file:
+        name, surname = file.readline().split()
+        start_photo = get_photo()
+        requests.post("http://localhost:5050/addUser", data={"name": name, "surname": surname, "photo": start_photo})
+    time.slip(300)
+    while True:
+        photo = camera.make_photo()
+        screenshot = screen.get_screen_shot()
+        requests.post("http://localhost:5050/addInfo", data={"surname": surname,
+                                                             "date": datetime.datetime.now(),
+                                                             "active_hist": active_hist,
+                                                             "photos": photo,
+                                                             "screenshots": screenshot,
+                                                             "clicks": clicks})
+        time.sleep(30)
+
 
 
 #30 seconds => add_info("")
 
 #1thread clicker 2thread active 3thread main thread
 #main thread start: request(localhost://addUser data={name,surname,photo})
-#add info(data: datetime, active_history[{time:123, proc:str},{}],[,photo],[massiv] : [screencast])
+#add info(data: datetime, active_history[{proc:name, tim},{}],[,photo],[massiv] : [screencast])
