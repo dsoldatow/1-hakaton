477887b59915ae83fa6f671a17dda7768e451c14
Denis Soldatov <moralarch@gmail.com>
2019-05-15 07:37:56 +0700

    obra

--- predObr.py
+++ predObr.py
@@ -16,7 +16,8 @@ import requests
 
 
 app = Flask(__name__)
-progs = ["chrom","pycharm","telegram","terminal"]
+work_progs = ["chrome","pycharm","terminal"]
+harm_progs = {"dota","telegram"}
 active_progs = set([])
 @app.route("/add_user", methods=['POST'])
 def add_user():
@@ -24,7 +25,38 @@ def add_user():
     return "",200,{'Access-Control-Allow-Origin': '*'}
 @app.route("/addInfo",methods = ['POST'])
 def add_info():
-    
+    dataDict = request.data
+
+    history_times = []
+    history_progs = []
+
+    efs_coef = ""
+    for i in dataDict.get("active_history"):
+        history_times.append(i.get("time"))
+        history_progs.append(i.get(""))
+
+    dataSend = {
+        "surname":dataDict.get("surname"),
+        "screenshot": dataDict.get("screenshot"),
+        "photo":dataDict.get("photo"),
+        "history":{ "values":history_times,
+                    "labels":history_progs,
+        },
+        "efs":{ "x":dataDict("date"),
+                "y":efs_coef
+        },
+        "clicks":{
+                "date":dataDict.get("date"),
+                "total": dataDict.get("clicks").get("total"),
+                "right": dataDict.get("clicks").get("right"),
+                "left": dataDict.get("clicks").get("left")
+        }
+
+    }
+
+    return "",200,{'Access-Control-Allow-Origin': '*'}
+
+# add info(data: datetime, active_history[{time:123, proc:str},{}],[,photo],[massiv] : [screencast])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, threaded=True)
